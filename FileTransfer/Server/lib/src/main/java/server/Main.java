eda2e91992106feb37315c525918185cb7a99543
lyect <lyect1@gmail.com>
2022-10-12 01:56:59 +0700

    FileTransfer v1

---
+++ FileTransfer/Server/lib/src/main/java/server/Main.java
@@ -0,0 +1,66 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class Main {
+	
+	private static int PORT;
+	private static Logger LOGGER;
+	
+	private static void setUpLoggerConfiguration() throws SecurityException, IOException {
+		InputStream ins = Main.class.getResourceAsStream("/logger.properties");
+		LogManager.getLogManager().readConfiguration(ins);
+	}
+	
+	public static void main(String[] args) throws Exception, SecurityException, IOException {
+		
+		// Load configuration of the logger
+		setUpLoggerConfiguration();
+		
+		// Check if appropriate number of arguments is given
+		if (args.length < 1) {
+			throw new Exception("Not enough arguments! Please, specify server's listening port.");
+		}
+		if (args.length > 1) {
+			throw new Exception("Too many arguments! There must be only server's listening port.");
+		}
+		
+		// Get port on which server will listen
+		PORT = Integer.parseInt(args[0]);
+		// Get logger for class "Main"
+		LOGGER = Logger.getLogger(Main.class.getName());
+		
+		// Start thread with logic of the server
+		Thread serverThread = new Thread(new ServerRunnable(PORT));
+		serverThread.start();
+		
+		// Start busy-waiting of STOP command
+		System.out.println("Enter \"STOP\" when you want to stop");
+		BufferedReader terminalReader = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String inputCommand = terminalReader.readLine().strip();
+			if (inputCommand.equals("STOP")) {
+				break;
+			}
+		}
+		
+		LOGGER.log(Level.INFO, "Closing terminal reader...");
+		terminalReader.close();
+		LOGGER.log(Level.INFO, "Terminal reader is closed.");
+		
+		// Interrupt server
+		LOGGER.log(Level.INFO, "Interrupting server...");
+		serverThread.interrupt();
+		serverThread.join();
+		LOGGER.log(Level.INFO, "Server is interrupted.");
+	}
+
+}
---
+++ FileTransfer/Server/lib/src/main/java/server/ServerRunnable.java
@@ -0,0 +1,106 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServerRunnable implements Runnable {
+	
+	private int port;
+	
+	private ServerSocket serverSocket;
+	private ExecutorService clientThreadPool = Executors.newCachedThreadPool();
+	private static Logger LOGGER;
+	
+	public ServerRunnable(int _port) {
+		LOGGER = Logger.getLogger(ServerRunnable.class.getName());
+		port = _port;	
+	}
+	
+	// Method for server starting.
+	// If it completes normally, then server socket is opened.
+	private void startServer() throws IOException {
+		serverSocket = new ServerSocket(port);
+		serverSocket.setSoTimeout(5000);
+	}
+	
+	// Method for interrupting client handlers.
+	private void interruptClientHandlers() {
+		clientThreadPool.shutdownNow();
+	}
+	
+	// Method for server closing.
+	// If it completes normally, then server socket is closed
+	//	and all client handlers are interrupted.
+	private void closeServer() throws IOException {
+		interruptClientHandlers();
+		serverSocket.close();
+	}
+	
+	private void log(Level level, String message) {
+		LOGGER.log(
+			level, 
+			message
+		);
+	}
+	
+	@Override
+	public void run() {
+		
+		// Starting server
+		try {
+			log(Level.INFO, "Starting server...");
+			startServer();
+			log(Level.INFO, "Server started successfully! Server is listening on port " + Integer.toString(port) + ".");
+			System.out.println("Server started successfully! Server is listening on port " + Integer.toString(port) + ".");
+		} catch (IOException ioe) {
+			log(Level.SEVERE, "Failed to start server on port " + Integer.toString(port) + ".");
+			log(Level.SEVERE, ioe.getMessage());
+			System.out.println("Failed to start server on port " + Integer.toString(port) + ".");
+			ioe.printStackTrace();
+			return;
+		}
+		
+		while (true) {
+			
+			// Check if server is interrupted.
+			// And if it is, then stop accepting loop.
+			if (Thread.currentThread().isInterrupted()) {
+				break;
+			}
+			
+			try {
+				// Accept incoming connection and start new thread with
+				//	client handler for this connection
+				Socket clientSocket = serverSocket.accept();
+				clientThreadPool.execute(new ClientHandler(clientSocket));
+			} catch (SocketTimeoutException e) { // Accept is timed out.
+				// Don't care about timeout
+			} catch (IOException e) { // Something except timeout happened. It is not good.
+				log(Level.SEVERE, e.getMessage());
+				break;
+			}
+		}
+		
+		if (!Thread.currentThread().isInterrupted()) {
+			System.out.println("Server is down. Enter \"STOP\" command and check logs.");
+		}
+		
+		// Closing server
+		try {
+			log(Level.INFO, "Closing server...");
+			closeServer();
+			log(Level.INFO, "Server closed successfully!");
+		} catch (IOException ioe) {
+			log(Level.WARNING, "Failed to close server properly.");
+			log(Level.WARNING, ioe.getMessage());
+			return;
+		}
+	}
+
+}
